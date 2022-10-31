e3a94aab09d6f94acc96086d4eba4be9e515cc5b
Matt Mortek <hi@mattmortek.co>
2022-10-31 23:40:52 +0100

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://b2storefront.com',
-  baseUrl: '/docs/',
+  url: 'https://b2storefront.com/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
