1d23c95de6c3d70f0b36767f716ced7f2a1f79f0
Matt Mortek <hi@mattmortek.co>
2022-11-07 16:14:16 +0100

    Update baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://b2storefront.com/',
-  baseUrl: '/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
